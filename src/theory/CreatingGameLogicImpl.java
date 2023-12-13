785eb5692b0e4e4fb33780dba476895da28fe621
Samuel <samugit.projects@gmail.com>
2023-12-13 15:20:44 -0300

    Aula02 Criando estratégia implementação lógica do jogo

---
+++ src/theory/CreatingGameLogicImpl.java
@@ -0,0 +1,22 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class CreatingGameLogicImpl {
+
+    /*
+    *
+    * uma das classes mais importantes do jogo é a classe Word pois contém a palavra oculta e a atualização dos palpites do usuário
+    *
+    *
+    *              WORD
+    *                    CHANCE           TEXT = JAVA
+    *
+    *            -> _ _ _ _               -> J A V A    array que armazena a palavra real
+    *                                        0 1 2 3
+    *                                        | | | |
+    *              Tentativas                v v v v
+    *              A S J                 -> J  A _ A    array que armazena o caractere coringa e atualiza o status do jogador
+    *                                       0  1 2 3
+    *
+    * */
+}
