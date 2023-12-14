f6cc1046813d857004a6eebe1805be8f629daaf9
Samuel <samugit.projects@gmail.com>
2023-12-14 13:07:58 -0300

    Aula11 Implementando o input via teclado

--- src/hangman/io/Input.java
+++ src/hangman/io/Input.java
@@ -6,10 +6,15 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Scanner;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.isNull;
 
+@SuppressWarnings("ALL")
 public final class Input {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private Input() {
     }
@@ -27,5 +32,12 @@ public final class Input {
                  IOException e) { // multi catch, para tratar mais de um tipo de exceção no mesmo bloco catch
             throw new RuntimeException("Error loading dictionary", e); // exceção aninhada, para exibir o problema e a causa em outro nível
         }
+    }
+
+    public static String readFromKeyboard(String message){
+        if (!isNull(message)) {
+            System.out.format("%s: ", message);
+        }
+        return SCANNER.nextLine();
     }
 }
